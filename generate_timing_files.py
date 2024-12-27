a346cbacd6542d0b982033909002446f9eb897da
L-lac <lena.lin.412@Gmail.com>
2024-12-27 01:42:28 -0500

    Update generate_timing_files.py

--- generate_timing_files.py
+++ generate_timing_files.py
@@ -9,11 +9,11 @@ df = pd.read_csv(file_path)
 
 #checks all the necessary columns are present in the file 
 if{'SetNum', 'SetType', 'stimulus_start_time', 'stimulus_end_time'}.issubset(df.columns):
-  df_selected = fd[['SetNum', 'SetType', 'stimulus_start_time', 'stimulus_end_time']].copy ()
+  df_selected = df[['SetNum', 'SetType', 'stimulus_start_time', 'stimulus_end_time']].copy ()
   # calculating response time 
   df_selected['responseRT'] = df_selected['stimulus_end_time'] - df_selected['stimulus_start_time']
   #Creating new Excel file 
   output_file = 'type_A_timing_file.xlsx'
   df_selected.to_excel(output_file, index = False)
-  print("Data has been processed and saved to new file)
+  print("Data has been processed and saved to new file.")
 else: print("Required columns are not present in the dataset.")
